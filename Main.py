c76600c9d9951b4bbfbdc79ff1d13db476f7ee0a
Kaden Yardley <kadenyardley@gmail.com>
2022-12-09 16:21:41 -0700

    added s3 access

--- Main.py
+++ Main.py
@@ -1,18 +1,25 @@
 import streamlit as st
 from model import GeneralModel
+from secrets_1 import access_key, secret_access_key
+import boto3
+import os
 
 
 def app():
 
     # Creating an object of prediction service
     pred = GeneralModel()
-
+    userTemp = st.slider('Complexity?', 1, 50, 1) * .02
+    userLength = st.slider('Length?', 1, 25, 1) * 100
     api_key = st.sidebar.text_input("APIkey", type="password")
+
     # Using the streamlit cache
     @st.cache
     def process_prompt(input):
 
-        return pred.model_prediction(input=input.strip() , api_key=api_key)
+        input2=input.strip()
+        return pred.model_prediction(input2, userTemp, userLength, api_key=api_key)
+ 
 
     if api_key:
 
@@ -32,6 +39,14 @@ def app():
         if st.button("Submit"):
             with st.spinner(text="In progress"):
                 report_text = process_prompt(input)
+                with open(str(input), 'w') as f:
+                    f.write(report_text)
+
+                client = boto3.client('s3', aws_access_key_id = access_key, aws_secret_access_key = secret_access_key)
+
+                upload_file_bucket = 'kadenfinalproject'
+                upload_file_key = str(input) + ".txt"
+                client.upload_file(str(input), upload_file_bucket, upload_file_key)
                 st.markdown(report_text)
     else:
         st.error("🔑 Please enter API Key")
